7b9a77c78fdda9aa8529c22ebe4638da457f9ad5
Unknown <andrey.bogomolov@tryggel.com>
2019-06-30 08:25:27 +0200

    Filter Works

---
+++ app/client/pages/app/Verleih-E-Fahrraeder_EPSG4326_KML.js
@@ -0,0 +1,127 @@
+var Verleih_E_Fahrraeder = {
+   "type" : "FeatureCollection",
+   "features" : [
+      {
+         "type" : "Feature",
+         "properties" : {
+            "Verleiher" : "Utopiastadt gemeinnützige GmbH - Bahnhof Mirke"
+         },
+         "geometry" : {
+            "type" : "Point",
+            "coordinates" : [
+               7.14507175421639,
+               51.2667260101318
+            ]
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "Dickten Fahrräder"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "coordinates" : [
+               7.19314040131471,
+               51.2693565191014
+            ],
+            "type" : "Point"
+         }
+      },
+      {
+         "geometry" : {
+            "type" : "Point",
+            "coordinates" : [
+               7.08794956050676,
+               51.2383443217044
+            ]
+         },
+         "type" : "Feature",
+         "properties" : {
+            "Verleiher" : "pro-e-motion Wuppertal"
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "Skaterhalle Wicked Woods"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "coordinates" : [
+               7.22392476316177,
+               51.279476212216
+            ],
+            "type" : "Point"
+         }
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               7.12833656065487,
+               51.2618406188169
+            ],
+            "type" : "Point"
+         },
+         "type" : "Feature",
+         "properties" : {
+            "Verleiher" : "Zweirad Otto"
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "Flender Funways"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "coordinates" : [
+               7.16526536317193,
+               51.257458125372
+            ],
+            "type" : "Point"
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "IKEA-Einrichtungshaus"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "type" : "Point",
+            "coordinates" : [
+               7.25530092508475,
+               51.3092673431409
+            ]
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "RENT A BIKE WUPPERTAL ESKESBERG"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "coordinates" : [
+               7.10610975866701,
+               51.2578063703306
+            ],
+            "type" : "Point"
+         }
+      },
+      {
+         "properties" : {
+            "Verleiher" : "Christian Wolter"
+         },
+         "type" : "Feature",
+         "geometry" : {
+            "coordinates" : [
+               7.18870982164399,
+               51.2638499348627
+            ],
+            "type" : "Point"
+         }
+      }
+   ],
+   "name" : "Verleih-E-Fahrraeder_EPSG4326_KML"
+};
+
+export {
+  Verleih_E_Fahrraeder
+}
--- app/client/pages/app/filter.vue
+++ app/client/pages/app/filter.vue
@@ -1,9 +1,16 @@
 <template>
     <v-app>
         <v-container>
+            <v-text-field
+                    v-model="datasets_filter"
+                    label="Filter für Datasets"
+            ></v-text-field>
             <v-switch
+                    small
                     v-for="(val,key) in fdata"
-                    v-model="fshow[key]" :label="key"/>
+                    v-model="fshow[key]" :label="key"
+                    v-if="!datasets_filter||datasets_filter===''||(datasets_filter&&key.toLowerCase().includes(datasets_filter.toLowerCase()))"
+            />
             <no-ssr>
                 <l-map class="mini-map" :zoom="12" :center="center">
                     <l-tile-layer
@@ -15,29 +22,44 @@
                     <div v-for="(filter, fkey) in fdata" :key="fkey"
                          v-if="fshow[fkey]"
                     >
-                        <l-marker
-                                v-for="(val,key) in filter.features"
-                                :lat-lng="[
-                                val.geometry.coordinates[0],
-                                val.geometry.coordinates[1]
+                        <div v-for="(val,key) in filter.features" :key="key">
+                            <!--Markers for the Point-->
+                            <div v-if="val.geometry.type==='Point'">
+                                <l-marker :lat-lng="[
+                                val.geometry.coordinates[1],
+                                val.geometry.coordinates[0]
                                 ]"
-                        >
-                            <l-icon :icon-anchor=" [30/2, 0]">
-                                <v-img height="30" width="30"
-                                       contain
-                                       src="https://mundraub.org/modules/custom/mundraub_map/img/campaigns.svg"/>
-                            </l-icon>
-                            <l-popup>
-                                <v-layout column style="text-align: center">
+                                >
+                                    <l-icon :icon-anchor=" [30/2, 0]">
+                                        <v-img height="30" width="30"
+                                               contain
+                                               src="https://mundraub.org/modules/custom/mundraub_map/img/campaigns.svg"/>
+                                    </l-icon>
+                                    <l-popup>
+                                        <v-layout column style="text-align: center">
 
-                                    <v-flex v-for="(val1,key1) in val.properties">
-                                        {{key1}}:{{val1}}
-                                    </v-flex>
-                                </v-layout>
+                                            <v-flex v-for="(val1,key1) in val.properties">
+                                                {{key1}}:{{val1}}
+                                            </v-flex>
+                                        </v-layout>
 
-                            </l-popup>
+                                    </l-popup>
 
-                        </l-marker>
+                                </l-marker>
+                            </div>
+                            <!--Polygons for Lines and Polygons-->
+                            <div v-if="val.geometry.type!='Point'">
+                                <l-geo-json
+                                        :geojson="val"
+                                        :options-style="styleFunction"
+                                        :options="options"
+                                >
+
+                                </l-geo-json>
+                            </div>
+
+                        </div>
+
                     </div>
                     <!--END MARKERS FOR FILTERS-->
                 </l-map>
@@ -47,24 +69,123 @@
 </template>
 
 <script>
+  import axios from "axios";
+
   import {mundraub} from "./mundraub.js";
+  import {carsharing} from "./Carsharing_EPSG4326_KML.js";
+  import {charg_station_cars} from "./Ladestationen-E-Autos_EPSG4326_KML.js";
+  import {charg_station_bikes} from "./Ladestationen-E-Fahrraeder_EPSG4326_KML.js";
+  import {Radabstellanlagen} from "./Radabstellanlagen_EPSG4326_KML.js";
+  import {Schwebebahnhoefe} from "./Schwebebahnhoefe_EPSG4326_JSON.js";
+  import {Verleih_E_Fahrraeder} from "./Verleih-E-Fahrraeder_EPSG4326_KML.js";
+  import {Zugaenge_Bahntrassenradwege} from "./Zugaenge-Bahntrassenradwege-Punkte_EPSG4326_KML.js";
+
+
   export default {
 
     data: () => ({
       // Travel time describes the stage on which it is traveling
+      datasets_filter: null,
       fdata: {
         mundraub: mundraub,
+        carsharing: carsharing,
+        charg_station_cars: charg_station_cars,
+        charg_station_bikes: charg_station_bikes,
+        Radabstellanlagen: Radabstellanlagen,
+        Schwebebahnhoefe: Schwebebahnhoefe,
+        Verleih_E_Fahrraeder: Verleih_E_Fahrraeder,
+        Zugaenge_Bahntrassenradwege: Zugaenge_Bahntrassenradwege,
+        Naturschutzgebiete: null,
+        Radrouten: null,
+        Stadtbezirke: null,
+        // Tempo30:null
       },
       fshow: {
-        mundraub:false,
+        mundraub: false,
+        carsharing: false,
+        charg_station_cars: false,
+        charg_station_bikes: false,
+        Radabstellanlagen: false,
+        Schwebebahnhoefe: false,
+        Verleih_E_Fahrraeder: false,
+        Zugaenge_Bahntrassenradwege: false,
+        Naturschutzgebiete: false,
+        Radrouten: false,
+        Stadtbezirke: false,
+        // Tempo30: false,
       },
       center: [51.2917076298, 7.2510191991],
       loading: false,
     }),
     methods: {
+      calc: function () {
+        axios
+            .get("Radrouten_EPSG4326_KML.json")
+            .then(response => {
+              console.log("Loading:");
+              this.fdata.Radrouten = response.data;
+
+            })
+            .catch(error => {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorCode, errorMessage);
+              // ...
+            });
+        axios
+            .get("Naturschutzgebiete_EPSG4326_JSON.json")
+            .then(response => {
+              console.log("Loading:");
+              this.fdata.Naturschutzgebiete = response.data;
+
+            })
+            .catch(error => {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorCode, errorMessage);
+              // ...
+            });
+        axios
+            .get("Stadtbezirke_EPSG4326_JSON.json")
+            .then(response => {
+              console.log("Loading:");
+              this.fdata.Stadtbezirke = response.data;
+
+            })
+            .catch(error => {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorCode, errorMessage);
+              // ...
+            });
+        // axios
+        //     .get("Tempo30-Zonen_EPSG4326_KML.json")
+        //     .then(response => {
+        //       console.log("Loading:");
+        //
+        //       this.fdata.Tempo30 = response.data;
+        //       console.log(this.fdata.Tempo30);
+        //
+        //     })
+        //     .catch(error => {
+        //       // Handle Errors here.
+        //       var errorCode = error.code;
+        //       var errorMessage = error.message;
+        //       console.log(errorCode, errorMessage);
+        //       // ...
+        //     });
+
+      }
     },
     watch: {},
     created() {
+
+    },
+    mounted() {
+      this.calc();
     },
     computed: {
 
